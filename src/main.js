53d85da83f8b38739ea27682fab6d1c153973c22
Nobuhiko Matsuura <matsuura0831@gmail.com>
2022-03-12 09:08:21 +0000

    use google tag manager. #7

--- src/main.js
+++ src/main.js
@@ -2,9 +2,21 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import VueGtag from "vue-gtag";
 import Toaster from '@meforma/vue-toaster';
 import './index.css';
 
-createApp(App).use(store).use(router).use(Toaster).mount("#app");
+const app = createApp(App);
+app.use(store).use(router).use(Toaster);
+
+if(process.env.NODE_ENV === "production") {
+    app.use(VueGtag, {
+        config: {
+            id: "G-224YJ56XVV",
+        },
+    }, router);
+}
+
+app.mount("#app");
 
 // Toaster info: https://meforma.github.io/vue-toaster
