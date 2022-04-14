365d3b78b2c1acffcae98f77e674f7382adb5503
matsuura0831 <matsuura0831@gmail.com>
2022-04-14 15:50:13 +0900

    Revert "ME対応"

--- src/components/WeaponInput.vue
+++ src/components/WeaponInput.vue
@@ -1,9 +1,6 @@
 <template>
-  <div>
-    <h2 class="mb-4 ml-2 font-bold text-md border-b-2 border-green-700">精練効果</h2>
-    <div class="flex items-center mb-1" v-for="v in inputs_equip" :key="v.key">
-
-      <label class="inline-block flex-none w-32 mr-2 text-right font-bold text-gray-600">{{ v.label }}</label>
+    <div class="flex items-center mb-1" v-for="v in inputs" :key="v.key">
+      <label class="inline-block flex-none w-24 mr-2 text-right font-bold text-gray-600">{{ v.label }}</label>
       <input
           :type="getType(v.number)"
           class="flex-1 py-2 w-12 border-gray-400 focus:border-green-400 text-gray-600 placeholder-gray-400 outline-none"
@@ -14,22 +11,6 @@
           onclick="this.select();"
         />
     </div>
-
-    <h2 class="mb-4 ml-2 font-bold text-md border-b-2 border-green-700">改造効果</h2>
-    <div class="flex items-center mb-1" v-for="v in inputs_custom" :key="v.key">
-
-      <label class="inline-block flex-none w-32 mr-2 text-right font-bold text-gray-600">{{ v.label }}</label>
-      <input
-          :type="getType(v.number)"
-          class="flex-1 py-2 w-12 border-gray-400 focus:border-green-400 text-gray-600 placeholder-gray-400 outline-none"
-          :class="{ 'bg-gray-200': !v.editable, 'border-b-0': !v.editable, 'border-b-2': v.editable, }"
-          @input="updateValue(v.key, $event.target.value, v.number)"
-          :value="this.modelValue[v.key]"
-          :disabled="!v.editable"
-          onclick="this.select();"
-        />
-    </div>
-  </div>
 </template>
 
 <script>
@@ -41,15 +22,7 @@ export default {
   data() {
     return {
       selected: this.target,
-      inputs_equip: [
-        { key: 'skill_up',      label: 'スキルダメージUP[%]',  editable: 1, number: 1},
-        { key: 'skill_mul_up',  label: 'スキルダメージ倍率UP[%]',  editable: 1, number: 1},
-        { key: 'ignore_mdef',   label: 'スキル防御無視[%]',  editable: 1, number: 1},
-        { key: 'vcast_p',       label: 'スキル変動時間減少[%]',  editable: 1, number: 1},
-        { key: 'fcast_p',       label: 'スキル固定時間減少[%]',  editable: 1, number: 1},
-        { key: 'fcast_s',       label: 'スキル固定時間減少[s]',  editable: 1, number: 1},
-      ],
-      inputs_custom: [
+      inputs: [
         { key: 'custom_skill_up',    label: 'スキルダメージUP[%]',  editable: 1, number: 1},
       ]
     }
