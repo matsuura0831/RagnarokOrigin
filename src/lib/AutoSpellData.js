512eeb59fb92cfbffde01d7f1f7ac106d30c25b3
matsuura <matsuura0831@gmail.com>
2023-12-02 14:59:56 +0000

    impl new data.

--- src/lib/AutoSpellData.js
+++ src/lib/AutoSpellData.js
@@ -1,12 +1,14 @@
+import { MagicDamageHandler } from "@/lib/MagicDamage";
 
 class AutoSpellSkill {
-    static VERSION = [1, 0];    // major, minor
+    static VERSION = [2, 0];    // major, minor
 
     constructor(
-        name, element, level, mul, add, prob, hit, ct,
+        name, alias, element, level, mul, add, prob, hit, ct,
         target_skill, target_element,
     ) {
         this.name = name;
+        this.alias = alias;
         this.element = element;
         this.level = level;
         this.mul = mul;
@@ -27,7 +29,7 @@ class AutoSpellSkill {
     serialize() {
         return [
             ...AutoSpellSkill.VERSION,
-            this.name, this.element, this.level, this.mul, this.add, this.prob, this.hit, this.ct,
+            this.name, this.alias, this.element, this.level, this.mul, this.add, this.prob, this.hit, this.ct,
             this.target_skill, this.target_element,
         ];
     }
@@ -63,6 +65,7 @@ const DATA = [
             { level: 3, mul: 250, prob: 34, ct: 1, hit: 1 },
             { level: 2, mul: 200, prob: 32, ct: 1, hit: 1 },
             { level: 1, mul: 150, prob: 30, ct: 1, hit: 1 },
+            { level: 0, mul: 0, prob: 0, ct: 1, hit: 0 },
         ]
     },
     {
@@ -77,6 +80,7 @@ const DATA = [
             { level: 3, mul: 250, prob: 34, ct: 1, hit: 1 },
             { level: 2, mul: 200, prob: 32, ct: 1, hit: 1 },
             { level: 1, mul: 150, prob: 30, ct: 1, hit: 1 },
+            { level: 0, mul: 0, prob: 0, ct: 1, hit: 0 },
         ]
     },
     {
@@ -91,6 +95,7 @@ const DATA = [
             { level: 3, mul: 250, prob: 34, ct: 1, hit: 1 },
             { level: 2, mul: 200, prob: 32, ct: 1, hit: 1 },
             { level: 1, mul: 150, prob: 30, ct: 1, hit: 1 },
+            { level: 0, mul: 0, prob: 0, ct: 1, hit: 0 },
         ]
     },
     {
@@ -100,6 +105,7 @@ const DATA = [
         records: [
             { level: 10, mul: 550 * 5, prob: 100, ct: 0, hit: 5 },
             { level: 7, mul: 550 * 5, prob: 100, ct: 0, hit: 5 },
+            { level: 0, mul: 0, prob: 0, ct: 0, hit: 0 },
         ]
     },
     {
@@ -114,6 +120,7 @@ const DATA = [
             { level: 3, mul: 200, prob: 100, ct: 0, hit: 1 },
             { level: 2, mul: 150, prob: 100, ct: 0, hit: 1 },
             { level: 1, mul: 100, prob: 100, ct: 0, hit: 1 },
+            { level: 0, mul: 0, prob: 0, ct: 0, hit: 0 },
         ]
     },
     {
@@ -122,6 +129,7 @@ const DATA = [
         target_skill: 'ストームガスト',
         records: [
             { level: 10, mul: 100 * (4.5 / 0.5), prob: 100, ct: 0, hit: 4.5 / 0.5 },
+            { level: 0, mul: 0, prob: 0, ct: 0, hit: 0 },
         ]
     },
     {
@@ -130,27 +138,151 @@ const DATA = [
         target_element: "水",
         records: [
             { level: 1, mul: 0, prob: 100, ct: 1, hit: 1 }, // 倍率は武器の処理の中で上書きする
+            { level: 0, mul: 0, prob: 0, ct: 1, hit: 0 },
         ]
     },
+
+    {
+        name: "ピットマン",
+        alias: "ヘブンズドライブ",
+        element: "地",
+        records: [
+            { level:15, mul: 1000, add: 225, prob:10, ct: 0, hit: 1 },
+            { level: 1, mul: 1000, add: 225, prob: 5, ct: 0, hit: 1 },
+            { level: 0, mul:    0, add:   0, prob: 0, ct: 0, hit: 0 },
+        ],
+        tips: "ヘブンズドライブLv5取得済みとして計算します",
+    },
+    {
+        name: "レッドエルマ",
+        alias: "ウォーターボール",
+        element: "水",
+        records: [
+            { level:15, mul: 1500, add: 1005, prob:10, ct: 1, hit: 15 },
+            { level: 1, mul: 1500, add: 1005, prob: 5, ct: 1, hit: 15 },
+            { level: 0, mul:    0, add:    0, prob: 0, ct: 1, hit:  0 },
+        ],
+    },
+    {
+        name: "ブラッディナイトR",
+        alias: "メテオストーム",
+        element: "火",
+        records: [
+            { level: 15, mul: 220, add: 18, prob: 5, ct: 7, hit: 5 },
+            { level: 1, mul: 220, add: 18, prob: 3, ct: 7, hit: 5 },
+            { level: 0, mul:   0, add: 0, prob: 0, ct: 1, hit: 0 },
+        ],
+    },
+    {
+        name: "ブラッディナイト",
+        alias: "メテオストーム",
+        element: "火",
+        records: [
+            { level: 15, mul: 330, add: 63, prob: 10, ct: 7, hit: 7 },
+            { level: 1, mul: 330, add: 63, prob: 5, ct: 7, hit: 7 },
+            { level: 0, mul:   0, add: 0, prob: 0, ct: 1, hit: 0 },
+        ],
+    },
+    {
+        name: "ミュータントドラゴン",
+        alias: "ファイアードラゴンブレス",
+        element: "火",
+        records: [
+            { level: 15, mul: 400, add: 0, prob: 30, ct: 0, hit: 1 },
+            { level: 10, mul: 400, add: 0, prob: 30, ct: 2, hit: 1 },
+            { level: 5, mul: 400, add: 0, prob: 24, ct: 2, hit: 1 },
+            { level: 1, mul: 400, add: 0, prob: 20, ct: 2, hit: 1 },
+            { level: 0, mul:   0, add: 0, prob: 0, ct: 1, hit: 0 },
+        ],
+    },
+    {
+        name: "マーリン",
+        alias: "ライトニングボルト",
+        element: "風",
+        show: true,
+        records: [
+            { level: 1, mul: 200, add: 32, prob: 5, ct: 2, hit: 1 },
+            { level: 0, mul:   0, add:  0, prob: 0, ct: 2, hit: 0 },
+        ],
+    },
+    {
+        name: "ボールステッカー",
+        alias: "ウォーターボール",
+        element: "水",
+        show: true,
+        records: [
+            { level: 1, mul: 800, add: 250, prob: 3, ct: 0.5, hit: 15 },
+            { level: 0, mul:   0, add:   0, prob: 0, ct: 0.5, hit:  0 },
+        ],
+    },
+    {
+        name: "ピクニックハット",
+        alias: "ウォーターボール",
+        element: "水",
+        show: true,
+        records: [
+            { level: 1, mul: 800, add: 250, prob: 1, ct: 0.5, hit: 15 },
+            { level: 0, mul:   0, add:   0, prob: 0, ct: 0.5, hit:  0 },
+        ],
+    },
+    {
+        name: "アクアオーブ",
+        element: "水",
+        show: true,
+        records: [
+            { level: 1, mul: 350, add: 0, prob: 10, ct: 0, hit: 1 },
+            { level: 0, mul:   0, add: 0, prob:  0, ct: 0, hit: 0 },
+        ],
+    },
+
+
 ];
 
 const CONVERT_DATA = {};
-DATA.forEach(({ name, element, records, target_skill = undefined, target_element = undefined }) => {
+const CONVERT_DISPLAY_DATA = {};
+DATA.forEach(({ name, element, records, target_skill = undefined, target_element = undefined, show = false, tips = '', alias = '' }) => {
     let m = {};
 
     records.forEach(({ level, mul, prob, ct, hit, add = 0 }) => {
-        m[level] = new AutoSpellSkill(name, element, level, mul, add, prob, hit, ct, target_skill, target_element);
+        let instance = new AutoSpellSkill(name, alias, element, level, mul, add, prob, hit, ct, target_skill, target_element);
+
+        m[level] = {
+            instance: instance,
+            tips: tips,
+            handler() {
+                return new class extends MagicDamageHandler {
+                    run(status, ismin, ismax) {
+                        if(level > 0) {
+                            if(name in status.pursuits) {
+                                // 同名がある場合は確率を加算する
+                                status.pursuits[name].prob += instance.prob;
+                            } else {
+                                status.pursuits[name] = instance.clone();
+                            }
+                        }
+                    }
+                }
+            },
+        }
     });
+
     CONVERT_DATA[name] = m;
+    if(show) CONVERT_DISPLAY_DATA[name] = m;
 })
 
 export default {
-    data: CONVERT_DATA,
+    data: CONVERT_DISPLAY_DATA,
     clazz: AutoSpellSkill,
 
     getAutoSpell(name, level) {
         const m = CONVERT_DATA[name];
-        const lv = level || Object.keys(m).sort((a, b) => b - a)[0];
-        return m[lv].clone();
+        const lv = level || 0;
+        return m[lv].instance.clone();
+    },
+    getHandler({ name, level }) {
+        return CONVERT_DATA[name][level].handler();
+    },
+    getTips({name, level}) {
+        return CONVERT_DATA[name][level].tips;
     }
 }
