d9abfeaa90addda95299967d9f5293a073f13a79
matsuura0831 <matsuura0831@gmail.com>
2024-04-25 11:17:19 +0000

    fix element skill_add and fix_cast_div.

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
     parser: "@babel/eslint-parser"
   },
   rules: {
+    "no-constant-condition": "off",
     "no-unused-vars": "off",
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
